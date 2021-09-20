73a6da9e80c4530f25e7084be9e02046a0a2cf93
dobadov3 <dobadov3@gmail.com>
2021-09-21 00:46:58 +0700

    Mobile Css

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,25 +1,46 @@
-import React from 'react';
-import { Link } from "react-router-dom";
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import Logo from '../assets/images/LogoHeader.png';
- 
+import { Collapse } from 'reactstrap';
+
 export default function Header() {
+    const [isOpen, setIsOpen] = useState(false);
+
     return (
         <header>
-            <div className='header-left'>
-                <Link to='/'>
-                    <img src={Logo} alt='' />
-                </Link>
-                <Link to='/'>
-                    <p>Mòe</p>
-                </Link>
+            <div className="desktop-wrapper">
+                <div className="header-left">
+                    <Link to="/">
+                        <img src={Logo} alt="" />
+                    </Link>
+                    <Link to="/">
+                        <p>Mòe</p>
+                    </Link>
+                </div>
+                <div className="header-right">
+                    <i className="fas fa-user-circle"></i>
+                    <span>
+                        <Link to="/login">Login</Link> / <Link to="/register">Register</Link>
+                    </span>
+                    <i className="fal fa-bars bar" onClick={() => setIsOpen(!isOpen)}></i>
+                </div>
             </div>
-            <div className='header-right'>
-                <i className='fas fa-user-circle'></i>
-                <span>
-                    <Link to='/login'>Login</Link> /{" "}
-                    <Link to='/register'>Register</Link>
-                </span>
-            </div>
+            <Collapse className="header-collapse" isOpen={isOpen}>
+                <ul>
+                    <li>
+                        <i className="fas fa-user"></i>
+                        <Link to="/login">Login</Link>
+                    </li>
+                    <li>
+                        <i className="fas fa-user-plus"></i>
+                        <Link to="/register">Register</Link>
+                    </li>
+                    <li>
+                        <i className="fas fa-sign-out-alt"></i>
+                        <Link to="/login">Logout</Link>
+                    </li>
+                </ul>
+            </Collapse>
         </header>
     );
 }
--- src/components/MainHeader.jsx
+++ src/components/MainHeader.jsx
@@ -1,13 +1,14 @@
 import React, { useState } from 'react';
-import { Container } from 'reactstrap';
+import { Collapse } from 'reactstrap';
 import LogoHeader from '../assets/images/MainLogoHeader.png';
 import { Link } from 'react-router-dom';
 import classnames from 'classnames';
 
 export default function MainHeader() {
     const [isHover, setIsHover] = useState(false);
+    const [isOpen, setIsOpen] = useState(false);
 
-    console.log(isHover)
+    console.log(isHover);
 
     const subClassName = classnames('sub-wrapper', {
         active: isHover === true,
@@ -15,71 +16,122 @@ export default function MainHeader() {
 
     return (
         <header id="main-header">
-            <div className="main-header-item left">
-                <img src={LogoHeader} alt="" />
-                <Link to="/home">
-                    <span>AAPet</span>
-                </Link>
-            </div>
-            <div className="main-header-item middle">
-                <Link to="/home">Home</Link>
-                <Link
-                    to="/home"
-                    className="has-item"
+            <div className="main-header-desktop-wrapper">
+                <div className="main-header-item left">
+                    <img src={LogoHeader} alt="" />
+                    <Link to="/home">
+                        <span>AAPet</span>
+                    </Link>
+                </div>
+                <div className="main-header-item middle">
+                    <Link to="/home">Home</Link>
+                    <Link
+                        to="/home"
+                        className="has-item"
+                        onMouseEnter={() => setIsHover(true)}
+                        onMouseLeave={() => setIsHover(false)}
+                    >
+                        Item <i className="fas fa-chevron-down"></i>
+                    </Link>
+
+                    <Link to="/home">Pet</Link>
+                </div>
+                <div className="main-header-item right">
+                    <i className="fas fa-user-circle"></i>
+                    <span>
+                        <Link to="/login">Login</Link> / <Link to="/register">Register</Link>
+                    </span>
+                    <i className="fal fa-shopping-cart"></i>
+                </div>
+                <div
+                    className={subClassName}
                     onMouseEnter={() => setIsHover(true)}
                     onMouseLeave={() => setIsHover(false)}
                 >
-                    Item <i className="fas fa-chevron-down"></i>
-                </Link>
+                    <div className="sub-item">
+                        <Link>Dresses</Link>
+                        <Link>
+                            <span>Outdoor</span>
+                        </Link>
+                        <Link>
+                            <span>Indoor</span>
+                        </Link>
+                        <Link>
+                            <span>Sport</span>
+                        </Link>
+                    </div>
+                    <div className="sub-item">
+                        <Link>Food</Link>
+                        <Link>
+                            <span>Royal</span>
+                        </Link>
+                        <Link>
+                            <span>Hill's</span>
+                        </Link>
+                        <Link>
+                            <span>Purina</span>
+                        </Link>
+                    </div>
+                    <div className="sub-item">
+                        <Link>Accessories</Link>
+                        <Link>
+                            <span>Groom</span>
+                        </Link>
+                        <Link>
+                            <span>Nail cutter</span>
+                        </Link>
+                    </div>
+                    <div className="sub-item">
+                        <Link>Colar Belt</Link>
+                        <Link>
+                            <span>Leather</span>
+                        </Link>
+                        <Link>
+                            <span>Fabrics</span>
+                        </Link>
+                    </div>
+                    <div className="sub-item">
+                        <Link>Leashes</Link>
+                        <Link>
+                            <span>Chain</span>
+                        </Link>
+                        <Link>
+                            <span>Rubber</span>
+                        </Link>
+                        <Link>
+                            <span>Leather</span>
+                        </Link>
+                    </div>
+                    <div className="sub-item">
+                        <Link>Bowl</Link>
+                        <Link>
+                            <span>Steel</span>
+                        </Link>
+                        <Link>
+                            <span>Plastic</span>
+                        </Link>
+                    </div>
+                </div>
+                <i className="fal fa-bars bar" onClick={() => setIsOpen(!isOpen)}></i>
+            </div>
 
-                <Link to="/home">Pet</Link>
-            </div>
-            <div className="main-header-item right">
-                <i className="fas fa-user-circle"></i>
-                <span>
-                    <Link to="/login">Login</Link> / <Link to="/register">Register</Link>
-                </span>
-                <i className="fal fa-shopping-cart"></i>
-            </div>
-            <div
-                className={subClassName}
-                onMouseEnter={() => setIsHover(true)}
-                onMouseLeave={() => setIsHover(false)}
-            >
-                <div className="sub-item">
-                    <Link>Dresses</Link>
-                    <Link><span>Outdoor</span></Link>
-                    <Link><span>Indoor</span></Link>
-                    <Link><span>Sport</span></Link>
-                </div>
-                <div className="sub-item">
-                    <Link>Food</Link>
-                    <Link><span>Royal</span></Link>
-                    <Link><span>Hill's</span></Link>
-                    <Link><span>Purina</span></Link>
-                </div>
-                <div className="sub-item">
-                    <Link>Accessories</Link>
-                    <Link><span>Groom</span></Link>
-                    <Link><span>Nail cutter</span></Link>
-                </div>
-                <div className="sub-item">
-                    <Link>Colar Belt</Link>
-                    <Link><span>Leather</span></Link>
-                    <Link><span>Fabrics</span></Link>
-                </div>
-                <div className="sub-item">
-                    <Link>Leashes</Link>
-                    <Link><span>Chain</span></Link>
-                    <Link><span>Rubber</span></Link>
-                    <Link><span>Leather</span></Link>
-                </div>
-                <div className="sub-item">
-                    <Link>Bowl</Link>
-                    <Link><span>Steel</span></Link>
-                    <Link><span>Plastic</span></Link>
-                </div>
-            </div>
+            <Collapse isOpen={isOpen} className="main-header-collapse">
+                <ul>
+                    <li>Home</li>
+                    <li>
+                        Item <i className="fas fa-chevron-down"></i>
+                    </li>
+                    <li>Pet</li>
+                    <li>
+                        <span>
+                            <Link to="/login">Login</Link> / <Link to="/register">Register</Link>
+                        </span>
+                    </li>
+                    <li>
+                        <i className="fal fa-shopping-cart"></i>
+                    </li>
+                </ul>
+            </Collapse>
         </header>
     );
 }
