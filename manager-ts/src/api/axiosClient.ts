82ae6474db7f3381ee54f52d75231915d987429b
dobadov3 <dobadov3@gmail.com>
2021-10-07 01:46:28 +0700

    Add typescript

---
+++ manager-ts/src/api/axiosClient.ts
@@ -0,0 +1,29 @@
+import axios from 'axios';
+import queryString from 'query-string';
+
+const axiosClient = axios.create({
+    baseURL: 'http://localhost:3500',
+    headers: {
+        'Content-Type': 'application/json',
+        'type': 'aa-pet'
+    },
+    paramsSerializer: params => queryString.stringify(params),
+});
+
+axiosClient.interceptors.request.use(async (config) => {
+    //handle token here...
+
+    return config;
+});
+
+axiosClient.interceptors.response.use((res) => {
+    if (res && res.data){
+        return res.data;
+    }
+
+    return res.data;
+}, err => {
+    throw err;
+});
+
+export default axiosClient;
---
+++ manager-ts/src/components/Layout/Header.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import "../../assets/sass/header.scss";
+import Avt from "../../assets/images/avatar.png";
+import { Link } from "react-router-dom";
+
+export const Header = () => {
+  return (
+    <div className="header-wrapper">
+      <span className="brand">AApet</span>
+      <div className="right-side">
+        <img src={Avt} className="img-fluid" alt="" />
+        <span className="user-name">
+          Your name <i className="fal fa-chevron-down"></i>
+        </span>
+        <div className="sub-options">
+          <Link to="/" className="my-2">
+            <i className="fal fa-user"></i>&nbsp;Your profile
+          </Link>
+          <Link to="/" className="my-2">
+            <i className="fal fa-cog"></i>&nbsp;Setting
+          </Link>
+          <Link to="/" className="my-2">
+            <i className="fal fa-sign-out-alt"></i>&nbsp;Logout
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
